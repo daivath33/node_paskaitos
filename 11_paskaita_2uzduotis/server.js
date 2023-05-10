14acd2ec1bc477efa680379d6c59aca14a997ce0
daiva <daivath33@gmail.com>
2023-05-10 10:19:41 +0300

    added routes

--- 11_paskaita_2uzduotis/server.js
+++ 11_paskaita_2uzduotis/server.js
@@ -17,89 +17,7 @@ client
   })
   .catch((error) => console.log('No connection...', error));
 
-//CATEGORIES
-app.get('/categories', async (req, res) => {
-  try {
-    const con = await client.connect();
-    const data = await con.db(DB).collection('categories').find().toArray();
-    await con.close();
-    res.send(data);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-app.get('/categoryvalue', async (req, res) => {
-  try {
-    const con = await client.connect();
-    const data = await con
-      .db(DB)
-      .collection('categories')
-      .aggregate([
-        {
-          $lookup: {
-            from: 'products',
-            localField: '_id',
-            foreignField: 'categoryId',
-            as: 'products',
-          },
-        },
-        // {
-        //   $group: {
-        //     _id: { title: '$title' },
-        //     totalAmount: { $sum: '$price' },
-        //   },
-        // },
-        // {
-        //   $group: {
-        //     _id: '$_id',
-        //     // size: {
-        //     //   $sum: '$array',
-        //     // },
-        //   },
-        // },
-        // { $group: { _id: {category: '$title} }, totalAmount: { $sum: '$price' }}
-
-        //'$title', products: {totalPrice: { $sum: '$price' } } },
-      ])
-      .toArray();
-    await con.close();
-
-    const result = data.map(
-      ({ title, products }) =>
-        `${title}: ${products
-          .map((el) => el.price)
-          .reduce((acc, curr) => acc + curr, 0)} Eur`
-    );
-    res.send(result);
-    // res.send(result);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-app.get('/categoriesWithProducts', async (req, res) => {
-  try {
-    const con = await client.connect();
-    const data = await con
-      .db(DB)
-      .collection('categories')
-      .aggregate([
-        {
-          $lookup: {
-            from: 'products',
-            localField: '_id',
-            foreignField: 'categoryId',
-            as: 'products',
-          },
-        },
-      ])
-      .toArray();
-    await con.close();
-    res.send(data);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
+// POST / create categories
 app.post('/categories', async (req, res) => {
   try {
     const con = await client.connect();
@@ -119,7 +37,7 @@ app.post('/categories', async (req, res) => {
   }
 });
 
-//PRODUCTS
+// POST /create products with category ID
 app.post('/products/:id', async (req, res) => {
   try {
     const con = await client.connect();
@@ -141,6 +59,19 @@ app.post('/products/:id', async (req, res) => {
   }
 });
 
+// 4) GET /get all categories
+app.get('/categories', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con.db(DB).collection('categories').find().toArray();
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// 5) GET  / get all products with category
 app.get('/products', async (req, res) => {
   try {
     const con = await client.connect();
@@ -163,6 +94,38 @@ app.get('/products', async (req, res) => {
       .toArray();
     await con.close();
     res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// 6) GET / categoryvalue/ resulyatas: kiekvienos kategorijos produktu kainos sumos
+app.get('/categoryvalue', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con
+      .db(DB)
+      .collection('categories')
+      .aggregate([
+        {
+          $lookup: {
+            from: 'products',
+            localField: '_id',
+            foreignField: 'categoryId',
+            as: 'products',
+          },
+        },
+      ])
+      .toArray();
+    await con.close();
+
+    const result = data.map(
+      ({ title, products }) =>
+        `${title}: ${products
+          .map((el) => el.price)
+          .reduce((acc, curr) => acc + curr, 0)}`
+    );
+    res.send(result);
   } catch (error) {
     res.status(500).send(error);
   }
